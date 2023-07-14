3a838327aa2b88c5249bbe941afc05b6a09d0bb0
Madhu sudan Bhattarai <summerpalpali@gmail.com>
2023-07-14 21:18:19 +0545

    post created

--- Areas/Admin/Controllers/PostController.cs
+++ Areas/Admin/Controllers/PostController.cs
@@ -1,11 +1,29 @@
-﻿using MadhuBlog.ViewModel;
+﻿using AspNetCoreHero.ToastNotification.Abstractions;
+using MadhuBlog.Data;
+using MadhuBlog.Models;
+using MadhuBlog.ViewModel;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace MadhuBlog.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class PostController : Controller
     {
+        private readonly ApplicationDbContext _context;
+        public INotyfService _notification { get; }
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public PostController(ApplicationDbContext context, INotyfService notification, IWebHostEnvironment webHostEnvironment = null, UserManager<ApplicationUser> userManager = null)
+        {
+            _context = context;
+            _notification = notification;
+            _webHostEnvironment = webHostEnvironment;
+            _userManager = userManager;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -19,7 +37,47 @@ namespace MadhuBlog.Areas.Admin.Controllers
         [HttpPost]
         public async Task<IActionResult> Create(CreatePostVM vm)
         {
-            return View();
+            if (!ModelState.IsValid)
+            {
+                return View(vm);
+            }
+            //get logged in user id
+            var loggedInUser = await _userManager.Users.FirstOrDefaultAsync(x => x.UserName == User.Identity!.Name);
+
+            var post = new Post();
+            post.Title = vm.Title;
+            post.Description = vm.Description;
+            post.ShortDescription = vm.ShortDescription;
+            post.ApplicationUserId = loggedInUser!.Id;
+
+            if (post.Title != null)
+            {
+                string slug = vm.Title!.Trim();
+                slug = slug.Replace(" ", "-");
+                post.Slug = slug + "-" + Guid.NewGuid();
+            }
+
+            if (vm.Thumbnail != null)
+            {
+                post.ThumbnailUrl = UploadImage(vm.Thumbnail);
+            }
+
+            await _context.Posts!.AddAsync(post);
+            await _context.SaveChangesAsync();
+            _notification.Success("Post Created Successfully");
+            return RedirectToAction("Index");
+        }
+        private string UploadImage(IFormFile file)
+        {
+            string uniqueFileName = "";
+            var folderPath = Path.Combine(_webHostEnvironment.WebRootPath, "thumbnails");
+            uniqueFileName = Guid.NewGuid().ToString() + "_" + file.FileName;
+            var filePath = Path.Combine(folderPath, uniqueFileName);
+            using (FileStream fileStream = System.IO.File.Create(filePath))
+            {
+                file.CopyTo(fileStream);
+            }
+            return uniqueFileName;
         }
     }
 }
