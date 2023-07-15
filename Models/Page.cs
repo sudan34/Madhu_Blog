c2f0edf0b67c47f5b2a3cc7a55f1da35e2f75f55
Madhu sudan Bhattarai <summerpalpali@gmail.com>
2023-07-15 08:10:04 +0545

    page controller added

--- Models/Page.cs
+++ Models/Page.cs
@@ -7,5 +7,7 @@
         public string? ShortDescription { get; set; }
         public string? Description { get; set;}
         public string? Slug { get; set;}
+        public string? ThumbnailUrl { get; set; }
+
     }
 }
