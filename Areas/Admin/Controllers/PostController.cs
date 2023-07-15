b427658f598b222b2212bdccf273961cc6da67d6
Madhu sudan Bhattarai <summerpalpali@gmail.com>
2023-07-15 07:02:28 +0545

    post delete

--- Areas/Admin/Controllers/PostController.cs
+++ Areas/Admin/Controllers/PostController.cs
@@ -91,6 +91,25 @@ namespace MadhuBlog.Areas.Admin.Controllers
             _notification.Success("Post Created Successfully");
             return RedirectToAction("Index");
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var post = await _context.Posts!.FirstOrDefaultAsync(x => x.Id == id);
+
+            var loggedInUser = await _userManager.Users.FirstOrDefaultAsync(x => x.UserName == User.Identity!.Name);
+            var loggedInUserRole = await _userManager.GetRolesAsync(loggedInUser!);
+
+            if (loggedInUserRole[0] == WebsiteRoles.WebsiteAdmin || loggedInUser?.Id == post?.ApplicationUserId)
+            {
+                _context.Posts!.Remove(post!);
+                await _context.SaveChangesAsync();
+                _notification.Success("Post Deleted Successfully");
+                return RedirectToAction("Index", "Post", new { area = "Admin" });
+            }
+            return View();
+        }
+
         private string UploadImage(IFormFile file)
         {
             string uniqueFileName = "";
