1490262cfd4f2e7e832af592cb99a8682ca72c7b
Madhu sudan Bhattarai <summerpalpali@gmail.com>
2023-07-15 10:47:42 +0545

    making layout dynamic

--- Areas/Admin/Controllers/SettingController.cs
+++ Areas/Admin/Controllers/SettingController.cs
@@ -1,15 +1,102 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using AspNetCoreHero.ToastNotification.Abstractions;
+using MadhuBlog.Data;
+using MadhuBlog.Models;
+using MadhuBlog.ViewModel;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace MadhuBlog.Areas.Admin.Controllers
 {
     [Area("Admin")]
-    [Authorize(Roles ="Admin")]
+    [Authorize(Roles = "Admin")]
     public class SettingController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _context;
+        public INotyfService _notification { get; }
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        public SettingController(ApplicationDbContext context, INotyfService notification, IWebHostEnvironment webHostEnvironment)
         {
-            return View();
+            _context = context;
+            _notification = notification;
+            _webHostEnvironment = webHostEnvironment;
+        }
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var settings = _context.Settings!.ToList();
+            if (settings.Count > 0)
+            {
+                var vm = new SettingVM()
+                {
+                    Id = settings[0].Id,
+                    SiteName = settings[0].SiteName,
+                    Title = settings[0].Title,
+                    ShortDescription = settings[0].ShortDescription,
+                    ThumbnailUrl = settings[0].ThumbnailUrl,
+                    FacebookUrl = settings[0].FacebookUrl,
+                    GithubUrl = settings[0].GithubUrl,
+                    LinkedInUrl = settings[0].LinkedInUrl,
+                };
+                return View(vm);
+            }
+            var setting = new Setting()
+            {
+                SiteName = "Demo Name",
+            };
+            await _context.Settings!.AddAsync(setting);
+            await _context.SaveChangesAsync();
+            var createdSettings = _context.Settings!.ToList();
+            var createdVm = new SettingVM()
+            {
+                Id = createdSettings[0].Id,
+                SiteName = createdSettings[0].SiteName,
+                Title = createdSettings[0].Title,
+                ShortDescription = createdSettings[0].ShortDescription,
+                ThumbnailUrl = createdSettings[0].ThumbnailUrl,
+                FacebookUrl = createdSettings[0].FacebookUrl,
+                GithubUrl = createdSettings[0].GithubUrl,
+                LinkedInUrl = createdSettings[0].LinkedInUrl,
+            };
+            return View(createdVm);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Index(SettingVM vm)
+        {
+            if (!ModelState.IsValid) { return View(vm); }
+            var setting = await _context.Settings!.FirstOrDefaultAsync(x => x.Id == vm.Id);
+            if (setting == null)
+            {
+                _notification.Error("Something went wrong");
+                return View(vm);
+            }
+            setting.SiteName = vm.SiteName;
+            setting.Title = vm.Title;
+            setting.ShortDescription = vm.ShortDescription;
+            setting.FacebookUrl = vm.FacebookUrl;
+            setting.GithubUrl = vm.GithubUrl;
+            setting.LinkedInUrl = vm.LinkedInUrl;
+
+            if (vm.Thumbnail != null)
+            {
+                setting.ThumbnailUrl = UploadImage(vm.Thumbnail);
+            }
+            await _context.SaveChangesAsync();
+            _notification.Success("Setting updated succesfully");
+            return RedirectToAction("Index", "Setting", new { area = "Admin" });
+        }
+
+        private string UploadImage(IFormFile file)
+        {
+            string uniqueFileName = "";
+            var folderPath = Path.Combine(_webHostEnvironment.WebRootPath, "thumbnails");
+            uniqueFileName = Guid.NewGuid().ToString() + "_" + file.FileName;
+            var filePath = Path.Combine(folderPath, uniqueFileName);
+            using (FileStream fileStream = System.IO.File.Create(filePath))
+            {
+                file.CopyTo(fileStream);
+            }
+            return uniqueFileName;
         }
     }
 }
